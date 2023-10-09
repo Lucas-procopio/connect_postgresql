5197380226450e9da4775eee50580b88a6e4b90d
Lucas Procópio <lucas.procopio.ext@dasa.com.br>
2023-10-09 13:30:06 -0300

    add create_database.py

---
+++ create_database.py
@@ -0,0 +1,28 @@
+import psycopg2
+import os
+
+class Database():
+    conn = psycopg2.connect(
+        database="postgres",
+        user='postgres',
+        password=os.environ['password'],
+        host='localhost',
+        port=os.environ['port']
+    )    
+    conn.autocommit = True
+    cursor = conn.cursor() # Creating coursor object
+
+    def __init__(self, database=None, user=None, password=None, host=None, port=None, conn=None, cursor=None):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.conn = conn
+        self.cursor = cursor
+
+    def create_database(self, database_name):
+        sql = f'''CREATE database {database_name}'''
+        self.cursor.execute(sql)
+        print('Database has been created successfully!')
+        self.conn.close()
