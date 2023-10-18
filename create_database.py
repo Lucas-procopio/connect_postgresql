a5c462ea620fe42e4a4adb63b1362673502d453e
Lucas-procopio <lucasprocopio.econ@yahoo.com>
2023-10-18 20:40:35 -0300

    updated docker-composer file

--- create_database.py
+++ create_database.py
@@ -2,17 +2,11 @@ import psycopg2
 import os
 
 class Create_database():
-    conn = psycopg2.connect(
-        database="postgres",
-        user='postgres',
-        password= 'pwd', #os.environ['password'],
-        host='localhost',
-        port='5432' #os.environ['port']
-    )    
+    conn = psycopg2.connect(user="postgres", password='"pwd"', host="127.0.0.1", port="5432", database="postgres")
     conn.autocommit = True
-    cursor = conn.cursor() # Creating coursor object
-
-    def __init__(self, database=None, user=None, password=None, host=None, port=None, conn=None, cursor=None):
+    cursor=conn.cursor()
+    
+    def __init__(self, database=None, user=None, password=None, host=None, port=None, conn=conn, cursor=cursor):
         self.database = database
         self.user = user
         self.password = password
@@ -25,4 +19,10 @@ class Create_database():
         sql = f'''CREATE database {database_name}'''
         self.cursor.execute(sql)
         print('Database has been created successfully!')
+        self.conn.close()
+
+    def create_Table(self, table_name):#, campos:list, tipo_campos:list):
+        sql = f'''CREATE TABLE {table_name} (nome varchar);'''
+        self.cursor.execute(sql)
+        print('Table was created!')
         self.conn.close()
