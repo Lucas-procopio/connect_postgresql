77e572711c51ce804b1d8fe4a1828f6ec3f7f4fe
Lucas-procopio <lucasprocopio.econ@yahoo.com>
2023-10-18 22:05:59 -0300

    creating _gettingfield and _gettingfiles

--- create_database.py
+++ create_database.py
@@ -1,4 +1,6 @@
 import psycopg2
+import yaml
+from yaml.loader import SafeLoader
 import os
 
 class Create_database():
@@ -15,14 +17,29 @@ class Create_database():
         self.conn = conn
         self.cursor = cursor
 
-    def create_database(self, database_name):
-        sql = f'''CREATE database {database_name}'''
-        self.cursor.execute(sql)
-        print('Database has been created successfully!')
-        self.conn.close()
+    def _gettingfiles(path='./tables'):
+        files = [file for file in os.listdir('./tables')]
+        return files
 
-    def create_Table(self, table_name):#, campos:list, tipo_campos:list):
-        sql = f'''CREATE TABLE {table_name} (nome varchar);'''
-        self.cursor.execute(sql)
-        print('Table was created!')
-        self.conn.close()
+    def _gettingfield(fields):
+        print(fields)
+        with open(fields) as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            print(data)
+        return data
+    
+    def create_Table(self):
+        tables = self._gettingfiles()
+        for table_name in tables:
+            table_fields = self._gettingfield(table_name)
+            try:
+                sql = f'''CREATE TABLE {table_name} {table_fields}'''
+                self.cursor.execute(sql)
+                print('Table was created!')
+                self.conn.close()
+            except:
+                print(f'Table {table_name} exist!')
+
+if __name__ == '__main__':
+    database = Create_database()
+    database.create_Table()
