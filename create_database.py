d18a675ee00f07d355851ae286a5bb3c6ce87793
Lucas-procopio <lucasprocopio.econ@yahoo.com>
2023-10-18 22:39:28 -0300

    update yamls

--- create_database.py
+++ create_database.py
@@ -8,7 +8,7 @@ class Create_database():
     conn.autocommit = True
     cursor=conn.cursor()
     
-    def __init__(self, database=None, user=None, password=None, host=None, port=None, conn=conn, cursor=cursor):
+    def __init__(self, database=None, user=None, password=None, host=None, port=None, conn=conn, cursor=cursor, path='./tables'):
         self.database = database
         self.user = user
         self.password = password
@@ -16,22 +16,21 @@ class Create_database():
         self.port = port
         self.conn = conn
         self.cursor = cursor
+        self.path = path
 
-    def _gettingfiles(path='./tables'):
-        files = [file for file in os.listdir('./tables')]
+    def _gettingfiles(self):
+        files = [file for file in os.listdir(self.path)]
         return files
 
-    def _gettingfield(fields):
-        print(fields)
+    def _gettingfield(self, fields):
         with open(fields) as f:
             data = yaml.load(f, Loader=SafeLoader)
-            print(data)
         return data
     
     def create_Table(self):
         tables = self._gettingfiles()
         for table_name in tables:
-            table_fields = self._gettingfield(table_name)
+            table_fields = self._gettingfield(os.path.join(self.path, table_name))
             try:
                 sql = f'''CREATE TABLE {table_name} {table_fields}'''
                 self.cursor.execute(sql)
@@ -40,6 +39,9 @@ class Create_database():
             except:
                 print(f'Table {table_name} exist!')
 
-if __name__ == '__main__':
+def run():
     database = Create_database()
     database.create_Table()
+
+if __name__ == '__main__':
+    run()
